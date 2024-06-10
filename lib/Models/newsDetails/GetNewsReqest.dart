11376cbe5328a6da7877aff9289bfbdd24e0171f
ali72-20 <as20201118@gmail.com>
2024-06-10 04:05:04 +0300

    % use Future builder and refactor code

--- lib/Models/newsDetails/GetNewsReqest.dart
+++ lib/Models/newsDetails/GetNewsReqest.dart
@@ -3,42 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:news_app/Models/newsDetails/verticalListView.dart';
 import 'package:news_app/Services/NewsServieces.dart';
 
-import '../../DataCalsses/newsDetails.dart';
 
-class GetNewsList extends StatefulWidget {
-  const GetNewsList({super.key});
 
-  @override
-  State<GetNewsList> createState() => _getNewsListState();
-}
-
-class _getNewsListState extends State<GetNewsList> {
-  List<newsDetails> news = [];
-
-  @override
-  void initState() {
-    super.initState();
-    getNewsList();
-  }
-
-  bool isLoading = true;
-
-  Future<void> getNewsList() async {
-    news = await NewsServieces(Dio(), category: 'general').getNews();
-    isLoading = false;
-    setState(() {});
-  }
+class GetNewsList  extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? const SliverToBoxAdapter(
-            child: Center(child: CircularProgressIndicator()),
-          )
-        : news.isNotEmpty
-            ? verticalListView(news: news)
-            : const SliverToBoxAdapter(
-                child: Center(child: Text("Opps something wrond, try later!!")),
-              );
+    return FutureBuilder(
+        future: NewsServieces(Dio(), category: 'general').getNews(),
+        builder: (context, snapshot) {
+          return verticalListView(news: snapshot.data ?? []);
+        });
   }
 }
