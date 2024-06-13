bfdd09aba69e555ea9fa044294a7e6420c23e434
ali72-20 <as20201118@gmail.com>
2024-06-13 18:57:42 +0300

    + add refresh + finish app

--- lib/Views/categoryView.dart
+++ lib/Views/categoryView.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/Models/newsDetails/GetNewsReqest.dart';
 
-class categoryView extends StatelessWidget {
+import '../Models/newsDetails/GetNewsReqest.dart';
+
+class categoryView extends StatefulWidget {
   categoryView({super.key, required this.categoryName});
-
   String categoryName;
+  @override
+  State<categoryView> createState() => _categoryViewState();
+}
 
+class _categoryViewState extends State<categoryView> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          categoryName,
+          widget.categoryName,
           style: const TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [GetNewsList(name: categoryName)],
+      body: RefreshIndicator(
+        onRefresh: () async{
+          await Future.delayed(Duration(seconds: 2));
+            setState(() {
+              GetNewsList(name: widget.categoryName);
+            });
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: [GetNewsList(name: widget.categoryName)],
+          ),
         ),
       ),
     );
